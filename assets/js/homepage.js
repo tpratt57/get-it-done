81cd6d4470a06383f030135471fc20b1a2246c84
unknown <pratttrevor79@gmail.com>
2022-06-02 18:44:12 -0500

    adding ability to dynamically targert a specific user's repos

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,13 @@
-var getUserRepos = function(){
-    fetch("https://api.github.com/users/octocat/repos")
-};
+var getUserRepos = function(user){
+    //format the github api url
 
-getUserRepos();
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    //make request to the URL
+    fetch(apiUrl).then(function(response){
+        response.json().then(function(data){
+            console.log(data);
+        });
+    });
+};
+getUserRepos("tpratt57");
